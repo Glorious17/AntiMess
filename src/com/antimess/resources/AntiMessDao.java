385c64053a4ad2433fbb1878865ef9b4ddf1cef0
Jens <ichunddu180@hotmail.de>
2016-06-09 15:51:06 +0200

    Database connection

--- src/com/antimess/resources/AntiMessDao.java
+++ src/com/antimess/resources/AntiMessDao.java
@@ -1,18 +1,29 @@
 package com.antimess.resources;
 
-import java.awt.List;
 import java.sql.*;
 import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import javax.sql.DataSource;
+
+
 
 public class AntiMessDao implements AntiMessDaoInterface {
 	
 	private static ArrayList <String> Datenbank = new ArrayList<String>();
+	private DataSource ds = null;
 	private Connection conn;
 	
 	public AntiMessDao(){
 		try {
-			conn = DriverManager.getConnection("jdbc:mysql://localhost/");
-		} catch (SQLException e) {
+			Context initCtx = new InitialContext();;
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+			ds = (DataSource)envCtx.lookup("jdbc/AntiMess");
+			conn = ds.getConnection();
+		} catch (SQLException | NamingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
