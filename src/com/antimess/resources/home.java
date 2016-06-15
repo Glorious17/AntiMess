899c36b5cb7b5eca309d3bf928fe0b539c75eca3
Jens <ichunddu180@hotmail.de>
2016-06-15 11:33:02 +0200

    home

---
+++ src/com/antimess/resources/home.java
@@ -0,0 +1,32 @@
+package com.antimess.resources;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class home
+ */
+@WebServlet("/home")
+public class home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private AntiMessBusiness bus;
+       
+    public home() {
+        super();
+        bus = new AntiMessBusiness();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("ID: " + request.getSession().getId());
+		if(bus.isLogedIn(request.getSession().getId()))
+			response.sendRedirect("dashboard.html");
+		else
+			response.sendRedirect("index.html");
+	}
+
+}
